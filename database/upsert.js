c673f60449d589a5198bf0e5c2eb811d3b31a4cc
Jools Barnett <vitamins999@gmail.com>
2022-01-26 16:05:02 +0000

    Added extra checks for duplicate supermarket products on upsert

--- database/upsert.js
+++ database/upsert.js
@@ -3,6 +3,8 @@ const SupermarketProduct = require('../models/SupermarketProduct');
 const upsertIntoDatabase = async (newDrinksArr, supermarketName) => {
   newDrinksArr.forEach(async (item) => {
     try {
+      // Patch price and offer if
+      // a) Product Name matches
       await SupermarketProduct.query()
         .patch({
           price: item.price,
@@ -11,6 +13,28 @@ const upsertIntoDatabase = async (newDrinksArr, supermarketName) => {
         })
         .findOne({
           productName: item.productName,
+          supermarket: item.supermarket,
+        });
+      // b) Link text matches
+      await SupermarketProduct.query()
+        .patch({
+          price: item.price,
+          offer: item.offer,
+          updatedAt: new Date().toISOString(),
+        })
+        .findOne({
+          link: item.link,
+          supermarket: item.supermarket,
+        });
+      // c) Image link text matches
+      await SupermarketProduct.query()
+        .patch({
+          price: item.price,
+          offer: item.offer,
+          updatedAt: new Date().toISOString(),
+        })
+        .findOne({
+          image: item.image,
           supermarket: item.supermarket,
         });
     } catch (error) {
@@ -37,8 +61,10 @@ const upsertIntoDatabase = async (newDrinksArr, supermarketName) => {
     if (
       !newList.some(
         (s) =>
-          s.productName === item.productName &&
-          s.supermarket === item.supermarket
+          (s.productName === item.productName &&
+            s.supermarket === item.supermarket) ||
+          (s.link === item.link && s.supermarket === item.supermarket) ||
+          (s.image === item.image && s.supermarket === item.supermarket)
       )
     ) {
       filteredNewSupermarkets.push(item);
