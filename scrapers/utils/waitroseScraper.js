f55f06471a3cb282d8df5a99569c2f16dc8b8d46
Jools Barnett <vitamins999@gmail.com>
2022-01-26 12:58:36 +0000

    Bugfix: fixed issue with Waitrose product volume not being scraped co… (#17)
    * Bugfix: fixed issue with Waitrose product volume not being scraped correctly
    
    * Removed unneeded console.log call

--- scrapers/utils/waitroseScraper.js
+++ scrapers/utils/waitroseScraper.js
@@ -42,7 +42,7 @@ const waitroseScraper = async (url, drinkType, drinkSubtype, scrollNum = 1) => {
         return;
       }
 
-      const size = $(el).find('.size___2HSwr').first().text();
+      const size = $(el).find('.size___12Ngq').first().text();
       const productName = `${productNameText} ${size}`;
 
       const priceText = $(el)
